85bc719a725ff68bfa000ecd9c0202df889fab9e
Nilambar Sharma <nilambar@outlook.com>
2020-06-28 15:35:51 +0545

    Cleaning up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,59 @@
-// Config for theme.
-var rootPath  = './';
-var projectURL = 'http://staging.local/';
-
-// Gulp Nodes.
+// Load Gulp.
 var gulp = require( 'gulp' );
 
-var autoprefixer = require( 'gulp-autoprefixer' );
-var plumber      = require( 'gulp-plumber' );
+// CSS related plugins.
 var sass         = require( 'gulp-sass' );
-var sourcemaps   = require( 'gulp-sourcemaps' );
+var autoprefixer = require( 'gulp-autoprefixer' );
 
+// JS related plugins.
+var uglify       = require( 'gulp-uglify' );
+var babelify     = require( 'babelify' );
+var browserify   = require( 'browserify' );
+var source       = require( 'vinyl-source-stream' );
+var buffer       = require( 'vinyl-buffer' );
+
+// Utility plugins.
+var rename     = require( 'gulp-rename' );
+var plumber    = require( 'gulp-plumber' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+
+// Browsers related plugins.
 var browserSync = require( 'browser-sync' ).create();
 
+// Project URL.
+var projectURL = 'http://staging.local/';
+
+// Style related variables.
+var styleSRC = './src/sass/front/style.scss';
+var styleURL = './';
+var mapURL   = './';
+
+// JS related variables.
+var jsSRC        = './src/scripts/';
+var jsFront      = 'custom.js';
+var jsCustomizer = 'customizer.js';
+var jsNavigation = 'navigation.js';
+var jsFiles      = [ jsFront, jsCustomizer, jsNavigation ];
+var jsURL        = './assets/js/';
+
+// Image related variables.
+var imgSRC = './src/images/**/*';
+var imgURL = './assets/images/';
+
+// Watch related variables.
+var styleWatch = './src/sass/**/*.scss';
+var jsWatch    = './src/scripts/**/*.js';
+var imgWatch   = './src/images/**/*.*';
+
 gulp.task('scss', function () {
-    return gulp.src(rootPath + 'src/sass/front/style.scss')
-        .on('error', sass.logError)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(autoprefixer('last 4 version'))
-        .pipe(sourcemaps.write(rootPath))
-        .pipe(gulp.dest(rootPath));
+    return gulp.src( styleSRC )
+        .on( 'error', sass.logError )
+        .pipe( plumber() )
+        .pipe( sourcemaps.init() )
+        .pipe( sass() )
+        .pipe( autoprefixer( 'last 4 version' ) )
+        .pipe( sourcemaps.write( styleURL ) )
+        .pipe( gulp.dest( styleURL ) );
 });
 
 gulp.task( 'watch', function() {
